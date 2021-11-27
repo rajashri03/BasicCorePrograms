4c849e26a4474e837ba8c0e7cecee508a293e6d0
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-27 07:08:45 +0530

    Check Whether a Number is Even or Odd

---
+++ BasicCorePrograms/BasicCorePrograms/CheckEvenOdd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class CheckEvenOdd
+    {
+        int Num;
+        public int EvenOdd()
+        {
+            Console.WriteLine("Enter Number:");
+            Num = Convert.ToInt32(Console.ReadLine());
+            if (Num % 2 == 0)
+            {
+
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.Write("Number is odd");
+            }
+            return Num;
+        }
+    }
+}
---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BasicCoreProgram;
+class EvenOdd
+{
+    public static void Main()
+    {
+        Console.Write("*******----Program to Check Whether a Number is Even or Odd:----********\n");
+        CheckEvenOdd number = new CheckEvenOdd();
+        number.EvenOdd();
+
+    }
+}
