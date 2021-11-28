89e87077c9d11a31bef9d8752c469c9d32419b2c
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-28 13:08:55 +0530

    Number is Even or Odd

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -4,9 +4,17 @@ class EvenOdd
 {
     public static void Main()
     {
+<<<<<<< Q8_EvenOdd
         Console.Write("*******----Program to Check Whether a Number is Even or Odd:----********\n");
         CheckEvenOdd number = new CheckEvenOdd();
         number.EvenOdd();
 
+=======
+        Console.WriteLine("Check Whether a Number is Even Or not");
+         CheckEvenOdd number = new CheckEvenOdd();
+       
+       
+        
+>>>>>>> local
     }
 }
