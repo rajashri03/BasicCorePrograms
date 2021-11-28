1160b0a328a6a1cee722cf92ac1142e2bffa8edf
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-28 13:59:21 +0530

    Quotient and Remainder

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -3,26 +3,31 @@ using BasicCoreProgram;
 using CheckVowelConsonant;
 using LargeNum;
 using CheckSwapNumber;
+using CheckQR;
 class EvenOdd
 {
     public static void Main()
     {
         //For Even_Odd
-         CheckEvenOdd number = new CheckEvenOdd();
+        CheckEvenOdd number = new CheckEvenOdd();
         //For Vowel Consonant
         VowelConsonant characterr = new VowelConsonant();
         //For LargeNumber
         LargeNumber bignum = new LargeNumber();
         //For Swap Two Numbers
         SwapNum swapping = new SwapNum();
+
+        //For Quotient remainder
+        Quotient_Remainder quotrem = new Quotient_Remainder();
         
 
         Console.WriteLine("Select Number:\n" +
             "1.Check Whether a Number is Even or Odd\n" +
             "2.Check Whether an Alphabet is Vowel or Consonant\n" +
-            "3.Find the Largest Among Three Numbers\n"+
-            "4.Swap Two Numbers");
-            int option = Convert.ToInt32(Console.ReadLine());
+            "3.Find the Largest Among Three Numbers\n" +
+            "4.Swap Two Numbers\n"+
+            "5.Compute Quotient and Remainder\n");
+        int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
         {
             case 1:
@@ -36,6 +41,9 @@ class EvenOdd
                 break;
             case 4:
                 swapping.CheckSwap();
+                break;
+            case 5:
+                quotrem.QuotRem();
                 break;
             default:
                 Console.Write("Please Select Correct Number");
---
+++ BasicCorePrograms/BasicCorePrograms/Quotient_Remainder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckQR
+{
+    public class Quotient_Remainder
+    {
+        int Dividend, Divisor;
+        public void QuotRem()
+        {
+            Console.Write("****--Compute Quotient and Remainder--****\n\n");
+            Console.Write("Enter Dividend:");
+            Dividend = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Divisor:");
+            Divisor = Convert.ToInt32(Console.ReadLine());
+            int quotient = Dividend / Divisor;
+            int remainder = Dividend % Divisor;
+            Console.Write("****--Quotient and Remainder--****\n\n");
+            Console.Write("Quotient:"+quotient+"\n");
+            Console.Write("Remainder:" + remainder + "\n");
+        }
+
+    }
+}
