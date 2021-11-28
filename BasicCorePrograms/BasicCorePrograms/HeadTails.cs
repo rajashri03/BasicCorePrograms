0b5d7ad3a1c9c6f348dd15e8ddfd9cc9a76bcfdf
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-28 15:30:02 +0530

    Flip Coin and print percentage of Heads and Tails

---
+++ BasicCorePrograms/BasicCorePrograms/HeadTails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Head_Tails
+{
+    public class HeadsTails
+    {
+        int NumOFCount, TailCount = 0, HeadCount = 0, PerOfHead, PerOfTails;
+        public void CheckHeadTail()
+        {
+            Console.Write("*****----Flip Coin and print percentage of Heads and Tails----*****\n");
+            Console.Write("Enter No. of times you want to Flip the Coin:" + "\n");
+            NumOFCount = Convert.ToInt32(Console.ReadLine());
+            for (int j = 1; j <= NumOFCount; j++)
+            {
+                Random random_num = new Random();
+                int randomheadtail = random_num.Next(0, 1);
+                if (randomheadtail < 0.5)
+                {
+                    TailCount++;
+                }
+                else
+                {
+                    HeadCount++;
+                }
+
+                Console.Write("****---> Percentage of Head vs Tails<---****\n");
+                PerOfHead = HeadCount / NumOFCount * 100;
+                PerOfTails = TailCount / NumOFCount * 100;
+                Console.Write("Percentage of Head-->" + PerOfHead + "%\n");
+                Console.Write("Percentage of Tails-->" + PerOfTails + "%\n\n");
+
+            }
+        }
+    }
+}
--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -4,6 +4,7 @@ using CheckVowelConsonant;
 using LargeNum;
 using CheckSwapNumber;
 using CheckQR;
+using Head_Tails;
 class EvenOdd
 {
     public static void Main()
@@ -19,35 +20,43 @@ class EvenOdd
 
         //For Quotient remainder
         Quotient_Remainder quotrem = new Quotient_Remainder();
+        //For printing percentage of Heads and Tails
+        HeadsTails headtail = new HeadsTails();
 
-
-        Console.WriteLine("Select Number:\n" +
-            "1.Check Whether a Number is Even or Odd\n" +
-            "2.Check Whether an Alphabet is Vowel or Consonant\n" +
-            "3.Find the Largest Among Three Numbers\n" +
-            "4.Swap Two Numbers\n" +
-            "5.Compute Quotient and Remainder\n");
-        int option = Convert.ToInt32(Console.ReadLine());
-        switch (option)
+        while (true)
         {
-            case 1:
-                number.EvenOdd();
-                break;
-            case 2:
-                characterr.CkeckVowelConso();
-                break;
-            case 3:
-                bignum.Checkbig();
-                break;
-            case 4:
-                swapping.CheckSwap();
-                break;
-            case 5:
-                quotrem.QuotRem();
-                break;
-            default:
-                Console.Write("Please Select Correct Number");
-                break;
+            Console.WriteLine("Select Number:\n" +
+                "1.Check Whether a Number is Even or Odd\n" +
+                "2.Check Whether an Alphabet is Vowel or Consonant\n" +
+                "3.Find the Largest Among Three Numbers\n" +
+                "4.Swap Two Numbers\n" +
+                "5.Compute Quotient and Remainder\n" +
+                "6.Find percentage of Heads and Tails\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    number.EvenOdd();
+                    break;
+                case 2:
+                    characterr.CkeckVowelConso();
+                    break;
+                case 3:
+                    bignum.Checkbig();
+                    break;
+                case 4:
+                    swapping.CheckSwap();
+                    break;
+                case 5:
+                    quotrem.QuotRem();
+                    break;
+                case 6:
+                    headtail.CheckHeadTail();
+                    break;
+                default:
+                    Console.Write("Please Select Correct Number\n");
+                    break;
+            }
         }
     }
 }
